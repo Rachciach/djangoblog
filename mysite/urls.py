b188f8711f224871dc2c5d86e36779ec9023648d
Rafal <rafi2002@op.pl>
2022-11-06 18:29:14 +0100

    Zmiana ustawien na stronie

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
+    #path(r'^admin/(\d+)$', admin.site.urls), moga wystepwoac cyfry (\d), + cyfra musi wystąpić co najmniej raz, ^ poczatek tekstu, $ koniec tekstu
 ]
